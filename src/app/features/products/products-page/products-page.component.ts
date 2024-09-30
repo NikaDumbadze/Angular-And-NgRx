4112ff2eff294f111b278e8ccc96adc1bb3a6e7d
Nika Dumbadze <ndumbadze@betlive.com>
2024-09-30 14:28:26 +0400

    selecting state with selectors

--- src/app/features/products/products-page/products-page.component.ts
+++ src/app/features/products/products-page/products-page.component.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProductsService } from 'src/app/services/products.service';
 import { Product } from 'src/app/models/product.model';
-import { sumProducts } from 'src/app/utils/sum-products';
 import { ProductListComponent } from '../product-list/product-list.component';
 import { Store } from '@ngrx/store';
 import { ProductsAPIActions, ProductsPageActions } from '../state/products.actions';
 import { Observable } from 'rxjs';
+import { selectProducts, selectProductsLoading, selectProductsShowProductCode, selectProductsTotal } from '../state/products.selectors';
 
 @Component({
   selector: 'app-products-page',
@@ -18,7 +18,7 @@ import { Observable } from 'rxjs';
 })
 export class ProductsPageComponent {
   products$!: Observable<Product[]>;
-  total = 0;
+  total$!: Observable<number>;
   loading$!: Observable<boolean>;
   showProductCode$: any;
   errorMessage = '';
@@ -28,9 +28,11 @@ export class ProductsPageComponent {
     private readonly store: Store
   ) {
     this.store.subscribe(store => console.log('store', store));
-    this.showProductCode$ = this.store.select((state: any) => state.products.showProductCode);
-    this.loading$ = this.store.select((state: any) => state.products.loading);
-    this.products$ = this.store.select((state: any) => state.products.products);
+    this.showProductCode$ = this.store.select(selectProductsShowProductCode);
+    this.loading$ = this.store.select(selectProductsLoading);
+    this.products$ = this.store.select(selectProducts);
+    this.products$ = this.store.select(selectProducts);
+    this.total$ = this.store.select(selectProductsTotal);
   }
 
   ngOnInit() {
@@ -43,7 +45,6 @@ export class ProductsPageComponent {
     this.productsService.getAll().subscribe({
       next: (products) => {
         this.store.dispatch(ProductsAPIActions.productsLoadedSuccess({ products }));
-        this.total = sumProducts(products);
       },
       error: (error) => (this.errorMessage = error),
     });
